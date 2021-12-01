d6bd87747210df1223108039e359d326277ab30e
Christian Payne <christianpayne522@gmail.com>
2021-12-01 09:52:36 -0800

    Working on button styles.

--- src/App.svelte
+++ src/App.svelte
@@ -13,11 +13,11 @@
   import Toggles from './lib/toggles.svelte';
 
   let pages = {
-    test: Test,
-    buttons: Buttons,
-    modals: Modals,
-    snackbars: Snackbars,
-    toggles: Toggles
+    Buttons: Buttons,
+    Modals: Modals,
+    Toggles: Toggles,
+    Snackbars: Snackbars,
+    Test: Test,
   };
 
   let _page = pages[Object.keys(pages)[0]]
@@ -35,17 +35,10 @@
           <span class="ml-3 text-xl">Model Match Style Guide</span>
         </div>
         <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-          <button class="mr-5 hover:text-gray-900" on:click="{()=>{setPage("buttons")}}">Buttons</button>
-          <button class="mr-5 hover:text-gray-900" on:click="{()=>{setPage("modals")}}">Modals</button>
-          <button class="mr-5 hover:text-gray-900" on:click="{()=>{setPage("toggles")}}">Toggles</button>
-          <button class="mr-5 hover:text-gray-900" on:click="{()=>{setPage("snackbars")}}">Snackbars</button>
-          <button class="mr-5 hover:text-gray-900" on:click="{()=>{setPage("test")}}">Test</button>
+          {#each Object.keys(pages) as page }
+            <button class="mr-5 hover:text-gray-900 border-2 rounded-md px-4 py-2 hover:bg-blue-400" on:click="{()=>{setPage(page)}}">{page}</button>
+          {/each}
         </nav>
-        <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
-          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
-            <path d="M5 12h14M12 5l7 7-7 7"></path>
-          </svg>
-        </button>
       </div>
     </header>
 
--- src/lib/buttons.svelte
+++ src/lib/buttons.svelte
@@ -1,3 +1,17 @@
-<h1>
-  buttons
-</h1>
+<div class="h-48 flex flex-wrap content-center">
+  <button class="mm-primary">Primary</button>
+  <button class="px-4 py-2 m-2 bg-blue-400">Secondary</button>
+  <button class="px-4 py-2 m-2 bg-blue-400">Warning</button>
+  <button class="px-4 py-2 m-2 bg-blue-400">Cancel</button>
+</div>
+
+<style>
+  .mm-button {
+    @apply px-4 py-2 m-2;
+  }
+
+  .mm-primary {
+    /* @apply .mm-button; */
+    @apply bg-blue-400;
+  }
+</style>
