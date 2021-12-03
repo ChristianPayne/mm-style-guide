1002d482868abb6f569556f2d7703b6d21501391
Christian Payne <christianpayne522@gmail.com>
2021-12-03 13:01:21 -0800

    Working on side bar.

--- src/App.svelte
+++ src/App.svelte
@@ -28,16 +28,35 @@
 </script>
 
 <main class="{darkMode ? "dark": ""}">
-  <div class="min-h-screen bg-mm-background dark:bg-mm-background-dark dark:text-mm-text-dark flex">
+  <div class="min-h-screen bg-mm-background dark:bg-mm-background-dark text-mm-text dark:text-mm-text-dark flex">
     <nav class="bg-mm-background-trim dark:bg-mm-background-trim-dark shadow-2xl p-1 sm:p-4 justify-items-center items-center min-w-1/3
     max-w-1/3 sm:min-w-1/4 sm:max-w-1/4">
-      <div class="col-span-1fr md:flex font-medium items-center text-mm-text mb-4 cursor-pointer text-center justify-items-center" on:click={()=>{toggleDarkMode();}}>
+      <div class="col-span-1fr md:flex font-medium items-center mb-4 cursor-pointer text-center justify-items-center">
         <img src={mmLogo} class="w-12 md:mr-2">
-        <span class="text-xl text-mm-text dark:text-mm-text-dark">Model Match Style Guide</span>
+        <span class="text-xl">Model Match Style Guide</span>
       </div>
+      <button class="mr-5 mb-2 w-full {darkMode ? "bg-mm-purple" : ""}" on:click={()=>{toggleDarkMode();}}>Dark Mode
+        <!-- {darkMode ?
+          <svg class="w-6 h-6" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <circle cx="12" cy="12" r="5"></circle>
+            <line x1="12" y1="1" x2="12" y2="3"></line>
+            <line x1="12" y1="21" x2="12" y2="23"></line>
+            <line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line>
+            <line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line>
+            <line x1="1" y1="12" x2="3" y2="12"></line>
+            <line x1="21" y1="12" x2="23" y2="12"></line>
+            <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
+            <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
+          </svg> 
+          : 
+          <svg class="w-6 h-6" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+          </svg>
+        } -->
+      </button>
       <div class="flex flex-col">
         {#each Object.keys(pages) as page }
-          <button class="mr-5 rounded-md px-4 py-2 mb-2 ring-mm-blue hover:ring-1 hover:shadow-md text-center w-full {_pageName === page ? "bg-mm-blue text-mm-background" : ""}" on:click="{()=>{setPage(page)}}">{page}</button>
+          <button class="mr-5 mb-2 w-full {_pageName === page ? "bg-mm-blue text-mm-background" : ""}" on:click="{()=>{setPage(page)}}">{page}</button>
         {/each}
       </div>
     </nav>
@@ -45,3 +64,9 @@
     <svelte:component class="" this={_page} />
   </div>
 </main>
+
+<style>
+  button {
+    @apply rounded-md px-4 py-2 ring-mm-blue hover:ring-1 hover:shadow-md text-center;
+  }
+</style>
--- src/lib/core/colors.svelte
+++ src/lib/core/colors.svelte
@@ -16,12 +16,14 @@
   ]
 </script>
 
-<div class="px-4 grid grid-flow-row grid-cols-2 sm:grid-cols-4 lg:grid-cols-6">
+<div class="px-4 grid grid-flow-row grid-cols-2 sm:grid-cols-4 lg:grid-cols-6 gap-4">
   {#each colors as color}
-    <div class="p-2">
-      {color}
-      <div class="rounded-md bg-mm-{color} hover:bg-mm-{color}-hover dark:bg-mm-{color}-dark h-24 w-24"></div>
+    <div class="p-2 relative h-24 w-24">
+      <div class="absolute rounded-md bg-mm-{color} hover:bg-mm-{color}-hover dark:bg-mm-{color}-dark h-full w-full">
+        <h2 class="h-full w-full flex items-center justify-center">
+          {color}
+        </h2>
+      </div>
     </div>
   {/each}
-  <!-- <h1 class="bg-mm-background-trim">TEST</h1> -->
 </div>
